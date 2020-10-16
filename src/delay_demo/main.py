826205cfb10aca406c6f0a3b16b0baffc6943771
Matthew Clapp <itsayellow+dev@gmail.com>
2020-10-16 15:30:52 -0700

    Add flush to print.

--- src/delay_demo/main.py
+++ src/delay_demo/main.py
@@ -4,7 +4,7 @@ import time
 def cli():
     try:
         for i in range(15):
-            print(i)
+            print(i, flush=True)
             time.sleep(1)
     except KeyboardInterrupt:
         print("delay-demo caught the KeyboardInterrupt, yay!")
